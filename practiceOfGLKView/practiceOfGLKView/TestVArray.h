d176113edec9237ab121f0f144294d70fd9107b2
nakanomi <nakanomi@gmail.com>
2013-09-16 18:06:45 +0900

    頂点情報の実装テストクラス

---
+++ practiceOfGLKView/practiceOfGLKView/TestVArray.h
@@ -0,0 +1,13 @@
+//
+//  TestVArray.h
+//  practiceOfGLKView
+//
+//  Created by nakano_michiharu on 2013/09/16.
+//  Copyright (c) 2013年 nakano_michiharu. All rights reserved.
+//
+
+#import "VArrayBase.h"
+
+@interface TestVArray : VArrayBase
+
+@end
---
+++ practiceOfGLKView/practiceOfGLKView/TestVArray.m
@@ -0,0 +1,13 @@
+//
+//  TestVArray.m
+//  practiceOfGLKView
+//
+//  Created by nakano_michiharu on 2013/09/16.
+//  Copyright (c) 2013年 nakano_michiharu. All rights reserved.
+//
+
+#import "TestVArray.h"
+
+@implementation TestVArray
+
+@end
