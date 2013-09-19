80e2b6a53b6cf9e3a875f7f77b102ba3744200f2
nakanomi <nakanomi@gmail.com>
2013-09-19 18:29:38 +0900

    GLKViewControllerの派生でないビューコントローラーを使いたいので別のビューコントローラーを追加

---
+++ practiceOfGLKView/practiceOfGLKView/GameViewController2.h
@@ -0,0 +1,13 @@
+//
+//  GameViewController2.h
+//  practiceOfGLKView
+//
+//  Created by nakano_michiharu on 9/19/13.
+//  Copyright (c) 2013 nakano_michiharu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController2 : UIViewController
+
+@end
---
+++ practiceOfGLKView/practiceOfGLKView/GameViewController2.m
@@ -0,0 +1,38 @@
+//
+//  GameViewController2.m
+//  practiceOfGLKView
+//
+//  Created by nakano_michiharu on 9/19/13.
+//  Copyright (c) 2013 nakano_michiharu. All rights reserved.
+//
+
+#import "GameViewController2.h"
+
+@interface GameViewController2 ()
+
+@end
+
+@implementation GameViewController2
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
