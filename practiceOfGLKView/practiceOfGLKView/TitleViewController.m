9aba5136004b03330f0b8bb66179559519dcf342
nakanomi <nakanomi@gmail.com>
2013-10-04 17:37:32 +0900

    読み込むXibの名前がリファクタリング前だったのを修正

--- practiceOfGLKView/practiceOfGLKView/TitleViewController.m
+++ practiceOfGLKView/practiceOfGLKView/TitleViewController.m
@@ -41,7 +41,7 @@
 {
 	NSLog(@"%s", __PRETTY_FUNCTION__);
 	if ([sender isEqual:self.btnGame]) {
-		 GameViewController *controller = [[GameViewController alloc] initWithNibName:@"GameViewController2" bundle:nil];
+		 GameViewController *controller = [[GameViewController alloc] initWithNibName:@"GameViewController" bundle:nil];
 		//GameViewController *controller = [[GameViewController alloc] initWithNibName:@"GameViewController" bundle:nil];
 		[self.navigationController pushViewController:controller animated:YES];
 		[controller release];
