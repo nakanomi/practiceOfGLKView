8a40ba319db2e2e4a5522db620a34b5c8b413aa8
nakanomi <nakanomi@gmail.com>
2013-10-11 18:01:32 +0900

    CADisplayLinkのランループからはGLKTextureでのテクスチャ読み込みが失敗する事、一方GLKViewControllerのランループからはテクスチャが読み込めるらしい事が分かった 再びGLKViewControllerを使い、そのランループからテクスチャを読み込むテストをする

---
+++ practiceOfGLKView/practiceOfGLKView/GLKGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  GLKGameViewController.h
+//  practiceOfGLKView
+//
+//  Created by nakano_michiharu on 10/11/13.
+//  Copyright (c) 2013 nakano_michiharu. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface GLKGameViewController : GLKViewController
+
+@end
---
+++ practiceOfGLKView/practiceOfGLKView/GLKGameViewController.m
@@ -0,0 +1,38 @@
+//
+//  GLKGameViewController.m
+//  practiceOfGLKView
+//
+//  Created by nakano_michiharu on 10/11/13.
+//  Copyright (c) 2013 nakano_michiharu. All rights reserved.
+//
+
+#import "GLKGameViewController.h"
+
+@interface GLKGameViewController ()
+
+@end
+
+@implementation GLKGameViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
