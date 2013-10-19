53139934ace09aa883951020d9f534cb702aa173
nakanomi <nakanomi@gmail.com>
2013-10-19 16:31:51 +0900

    シェーダーを選択するためのテーブルビューコントローラーを追加

---
+++ practiceOfGLKView/practiceOfGLKView/SelShaderViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelShaderViewController.h
+//  practiceOfGLKView
+//
+//  Created by nakano_michiharu on 10/19/13.
+//  Copyright (c) 2013 nakano_michiharu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelShaderViewController : UITableViewController
+
+@end
