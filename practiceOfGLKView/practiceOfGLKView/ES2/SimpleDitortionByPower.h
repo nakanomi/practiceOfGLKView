435e384def7f502f1b1fbd28086d6bcbdefe78b5
nakanomi <nakanomi@gmail.com>
2013-11-17 16:36:07 +0900

    pow関数による歪みシェーダー追加

---
+++ practiceOfGLKView/practiceOfGLKView/ES2/SimpleDitortionByPower.h
@@ -0,0 +1,13 @@
+//
+//  SimpleDitortionByPower.h
+//  practiceOfGLKView
+//
+//  Created by nakano_michiharu on 2013/11/17.
+//  Copyright (c) 2013年 nakano_michiharu. All rights reserved.
+//
+
+#import "SimpleMultiTexture.h"
+
+@interface SimpleDitortionByPower : SimpleMultiTexture
+
+@end
---
+++ practiceOfGLKView/practiceOfGLKView/ES2/SimpleDitortionByPower.mm
@@ -0,0 +1,13 @@
+//
+//  SimpleDitortionByPower.m
+//  practiceOfGLKView
+//
+//  Created by nakano_michiharu on 2013/11/17.
+//  Copyright (c) 2013年 nakano_michiharu. All rights reserved.
+//
+
+#import "SimpleDitortionByPower.h"
+
+@implementation SimpleDitortionByPower
+
+@end
